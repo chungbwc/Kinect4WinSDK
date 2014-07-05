4785bcf60ff826fa4fc31194f12dab501d5cb520
Bryan Chung <chungbwc@gmail.com>
2014-07-05 23:07:48 +0800

    Upload of all codes
    All codes, including Eclipse, Visual Studio 2012

---
+++ vs2012/Kinect4WinSDK/Kinect4WinSDK/kinect4WinSDK_Kinect.cpp
@@ -0,0 +1,111 @@
+#include "stdafx.h"
+#include "kinect4WinSDK_Kinect.h"
+#include "Kinect4WinSDK.h"
+
+JNIEXPORT void JNICALL Java_kinect4WinSDK_Kinect_init
+	(JNIEnv* env, jobject obj)
+{
+	Kinect4WinSDK* kinect = new Kinect4WinSDK();
+	jclass cls;
+	jfieldID fid;
+
+	cls = env->GetObjectClass(obj);
+	fid = env->GetFieldID(cls, "ptr", "J");
+	env->SetLongField(obj, fid, (jlong) kinect);
+	env->DeleteLocalRef(cls);
+}
+
+JNIEXPORT jintArray JNICALL Java_kinect4WinSDK_Kinect_Nui_1GetImage
+	(JNIEnv* env, jobject obj)
+{
+	Kinect4WinSDK* kinect;
+	jclass cls;
+	jfieldID fid;
+	jintArray result;
+	
+	cls = env->GetObjectClass(obj);
+	fid = env->GetFieldID(cls, "ptr", "J");
+	kinect = (Kinect4WinSDK *) env->GetLongField(obj, fid);
+	result = env->NewIntArray(WIDTH*HEIGHT);
+	env->SetIntArrayRegion(result, 0, WIDTH*HEIGHT, (jint *) (kinect->Nui_GetImage()));
+
+	env->DeleteLocalRef(cls);
+	return result;
+}
+
+JNIEXPORT jintArray JNICALL Java_kinect4WinSDK_Kinect_Nui_1GetDepth
+	(JNIEnv* env, jobject obj)
+{
+	Kinect4WinSDK* kinect;
+	jclass cls;
+	jfieldID fid;
+	jintArray result;
+	
+	cls = env->GetObjectClass(obj);
+	fid = env->GetFieldID(cls, "ptr", "J");
+	kinect = (Kinect4WinSDK *) env->GetLongField(obj, fid);
+	result = env->NewIntArray(WIDTH*HEIGHT);
+	env->SetIntArrayRegion(result, 0, WIDTH*HEIGHT, (jint *) (kinect->Nui_GetDepth()));
+	env->DeleteLocalRef(cls);
+	return result;
+}
+
+JNIEXPORT jintArray JNICALL Java_kinect4WinSDK_Kinect_Nui_1GetMask
+  (JNIEnv* env, jobject obj)
+{
+	Kinect4WinSDK* kinect;
+	jclass cls;
+	jfieldID fid;
+	jintArray result;
+	
+	cls = env->GetObjectClass(obj);
+	fid = env->GetFieldID(cls, "ptr", "J");
+	kinect = (Kinect4WinSDK *) env->GetLongField(obj, fid);
+	result = env->NewIntArray(WIDTH*HEIGHT);
+	env->SetIntArrayRegion(result, 0, WIDTH*HEIGHT, (jint *) (kinect->Nui_GetMask()));
+	env->DeleteLocalRef(cls);
+	return result;
+}
+
+JNIEXPORT void JNICALL Java_kinect4WinSDK_Kinect_getFigure
+	(JNIEnv* env, jobject obj)
+{
+	Kinect4WinSDK* kinect;
+	jclass cls1;
+	jfieldID fid1;
+	jfieldID fid2;
+	jobject obj2;
+
+	cls1 = env->GetObjectClass(obj);
+	fid1 = env->GetFieldID(cls1, "ptr", "J");
+	kinect = (Kinect4WinSDK *) env->GetLongField(obj, fid1);
+
+	fid2 = env->GetFieldID(cls1, "joints", "[[[F");
+	obj2 = env->GetObjectField(obj, fid2);
+
+	jobjectArray result = reinterpret_cast<jobjectArray> (obj2);
+
+	for (int i=0; i<NUI_SKELETON_COUNT; i++) 
+	{
+		jobjectArray v1 = env->NewObjectArray(NUI_SKELETON_POSITION_COUNT+1, env->FindClass("[F"), 0);
+
+		for (int j=0; j<NUI_SKELETON_POSITION_COUNT+1; j++) 
+		{
+			jfloat temp[5];
+			jfloatArray v2 = env->NewFloatArray(5);
+			for (int k=0; k<5; k++) 
+			{
+				temp[k] = kinect->joints[i][j][k];
+			}
+
+			env->SetFloatArrayRegion(v2, 0, 5, temp);
+			env->SetObjectArrayElement(v1, j, v2);
+			env->DeleteLocalRef(v2);
+		}
+		env->SetObjectArrayElement(result, i, v1);
+		env->DeleteLocalRef(v1);
+	}
+	env->DeleteLocalRef(result);
+	env->DeleteLocalRef(obj2);
+	env->DeleteLocalRef(cls1);
+}
